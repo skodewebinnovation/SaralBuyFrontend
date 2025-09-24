f91c0458ae5fc0f3da92a3613dfda4d136282b83
ShubhamSharma20007 <shubhamsharma20007@gmail.com>
2025-09-25 00:04:04 +0530

    cng

---
+++ src/Pages/bidOverView.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BidOverView = () => {
+  return (
+    <div>BidOverView</div>
+  )
+}
+
+export default BidOverView
---
+++ src/services/cart.service.ts
@@ -0,0 +1,17 @@
+
+import instance from "@/lib/instance";
+
+class CartService{
+  addToCart(productId:string){
+    return instance.post('/cart/add',{productId},{
+      withCredentials:true
+    }).then(res => res.data)
+  };
+    getCart(){
+    return instance.get('/cart/get-cart',{
+      withCredentials:true
+    }).then(res => res.data?.data || res.data)
+  }
+
+}
+export default new CartService();
